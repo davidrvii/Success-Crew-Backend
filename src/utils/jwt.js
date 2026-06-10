ff0b60801b3141beb2d68bd253f1e52a2d9b2e93
davidrvii <davidrsitorus04@gmail.com>
2026-06-10 10:41:52 +0700

    Adjust Secret Key get in jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken')
 
-function generateToken(payload){
-    return jwt.sign(payload, process.env.SECRET_KEY)
+const SECRET_KEY = process.env.SECRET_KEY
+
+function generateToken(payload) {
+    return jwt.sign(payload, SECRET_KEY)
 }
 
 function verifyToken(token) {
-    return jwt.verify(token, process.env.SECRET_KEY)
+    return jwt.verify(token, SECRET_KEY)
 }
 
 module.exports = {
